d75f058f970a69ed0ccfb5365e3e2ec353231d05
Ashwani Yadav <yadav8800766165@gmail.com>
2025-07-13 10:32:56 +0530

    ready for deploy2

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
  import axios from "axios";
 
   const axiosInstance=axios.create({
-    baseURL: import.meta.env.MODE === "development" ?  "http://localhost:5000/api" :"/api",
+    baseURL: import.meta.env.MODE === "development" ?  "https://full-stack-chat-app-liart.vercel.app/api" :"/api",
     withCredentials:true,
  });
 
