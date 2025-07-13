9cee46faf45459557e4d4b4dfff7297dc5fd4d7c
Ashwani Yadav <yadav8800766165@gmail.com>
2025-07-13 12:28:12 +0530

    ready to deploy 5  index.js

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -75,7 +75,7 @@ export const login = async (req, res) => {
     });
   } catch (error) {
     console.log("Error in log in controller:", error);
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ error });
   }
 };
 
