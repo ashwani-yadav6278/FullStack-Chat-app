5dda1d79a604c938d9afbe614bc7287df80143c9
Ashwani Yadav <yadav8800766165@gmail.com>
2025-07-13 23:35:21 +0530

    ready 2  deploy

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
  import axios from "axios";
 axios.defaults.withCredentials = true;
   const axiosInstance=axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
     withCredentials:true,
  });
 
--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -5,7 +5,7 @@ import { io } from "socket.io-client";
 
 
 // Not include  `/api` here; Socket.IO needs the root server URL
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
 
 
 
